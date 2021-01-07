c3d114bc0a6968cd6a959c6a20b46f0116896fd0
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-01-07 12:10:03 +0100

    creation email.blade.php

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\EmailRequest;
+use App\Models\Email;
 
 class EmailController extends Controller
 {
-    //
+    public function getForm()
+    {
+        return \view('email');
+    }
+
+    public function postForm(EmailRequest $request)
+    {
+        $email = new Email;
+        $email->email = $request->input('email');
+        $email->save();
+
+        return \view('email_ok');
+    }
 }
---
+++ resources/views/email.blade.php
@@ -0,0 +1,21 @@
+@extends('templates.layout')
+
+@section('content')
+    <br>
+    <div class="offset-4 col-sm-4">
+		<div class="card card-info">
+			<div class="card-heading">Inscription à la lettre d'information</div>
+			<div class="card-body"> 
+                <form action="{{ route('storeEmail') }}" method="POST">
+                    {!! csrf_field() !!}
+                    <div class="form-group">
+                        <input type="email" name="email" id="email" class="form-control" placeholder="Entrez votre email" />
+                        {!! $errors->first('email', '<small class="help-block">:message</small>') !!}
+                    </div>
+
+                    <button type="submit" class="btn btn-info float-right">Envoyer !</button>
+                </form>
+			</div>
+		</div>
+	</div>
+@endsection
